7fccf2a2ffc47d601c64894d517ae742fc028dd0
deepweed <deepweedx@gmail.com>
2026-06-30 01:14:52 +0300

    updated modules

--- src/telegram/order/order.module.ts
+++ src/telegram/order/order.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { OrderController } from "./order.controller";
 import { OrderService } from "./order.service";
-import { EmailModule } from "src/email/email.module";
+import { EmailModule } from "../../email/email.module";
 
 @Module({
   imports: [EmailModule],
--- src/telegram/phone/phone.module.ts
+++ src/telegram/phone/phone.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { PhoneService } from "./phone.service";
 import { PhoneController } from "./phone.controller";
-import { EmailModule } from "src/email/email.module";
+import { EmailModule } from "../../email/email.module";
 
 @Module({
   imports: [EmailModule],
